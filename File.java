d3571fe93e5d72eddad90a9b177a4e8f15ed23f1
nikhithagirigi929 <nikhithagirigi929@gmail.com>
2022-11-03 09:53:42 +0530

    file was created by lahari

---
+++ File.java
@@ -0,0 +1,9 @@
+Class File
+{}[D
+[A[C
+[A[C[
+public static void main()[Dstring args[])
+{
+system.out.println(""[DFile");
+}
+}[A[S[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[S[B[B[B[B
